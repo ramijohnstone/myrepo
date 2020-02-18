9834485bdb269b0f2e0f4b8ab2cb76602619de04
JOHNSTONE Rami <johnstone.rami@univ-catholyon.fr>
2020-02-18 15:14:41 +0100

    encore une question

--- script.py
+++ script.py
@@ -4,8 +4,15 @@ print("nm, hbu?")
 print("OK BYE")
 print("bye")
 
-x = input('es-tu majeur?')
-if x =='y':
+Q1 = input('es-tu majeur?')
+
+if Q1 =='y':
     print('tu peux entré')
-else:
+elif Q1 == 'n':
     print('désolé, tu ne peux pas entré')
+Q2 = input('tu vas bien?')
+if Q2 =='y':
+    print('nickel')
+elif Q2 == 'n':
+    print('dommage')
+
