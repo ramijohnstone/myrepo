85c8bd52b60d0dee6817cc91ef5d6625e8e4e106
JOHNSTONE Rami <johnstone.rami@univ-catholyon.fr>
2020-02-18 15:05:59 +0100

    ajout d'une question

--- script.py
+++ script.py
@@ -3,3 +3,9 @@ print("what is up")
 print("nm, hbu?")
 print("OK BYE")
 print("bye")
+
+x = input('es-tu majeur?')
+if x =='y':
+    print('tu peux entré')
+else:
+    print('désolé, tu ne peux pas entré')
