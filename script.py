4b5ea7440a6da6b0c7c520a9aa61168a104be2a8
LATTIER Innocent <lattier.innocent@univ-catholyon.fr>
2020-03-03 14:25:59 +0100

    premiere modif sur le repo de rami git add script.py

--- script.py
+++ script.py
@@ -7,21 +7,21 @@ print("bye")
 print("hello again")
 
 
-Q1 = input('es-tu majeur?')
+Q1 = input('es-tu majeur? ')
 
-if Q1 =='y':
+if Q1 =='yes':
     print('tu peux entré')
-elif Q1 == 'n':
+elif Q1 == 'no':
     print('désolé, tu ne peux pas entré')
-Q2 = input('tu vas bien?')
-if Q2 =='y':
+Q2 = input('tu vas bien? ')
+if Q2 =='yes':
     print('nickel')
-elif Q2 == 'n':
+elif Q2 == 'no':
     print('dommage')
-Q3 = input('is pierre a beast?')
-if Q3 == 'y':
+Q3 = input('is pierre a beast? ')
+if Q3 == 'yes':
     print('false')
-elif Q3 == 'n':
+elif Q3 == 'no':
     print('True')
 
-c
+
