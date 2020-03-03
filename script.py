fca321860ce2ebdad833b5773b389d5f6ab87da7
CONGIU Gaétan <congiu.gaetan@univ-catholyon.fr>
2020-03-03 14:22:59 +0100

    Question sur Manu

--- script.py
+++ script.py
@@ -24,4 +24,11 @@ if Q3 == 'y':
 elif Q3 == 'n':
     print('True')
 
-c
+
+manu = int(input("Sur une échelle de 1 à 10 à quel point trouve tu manu con ?"))
+if manu <= 5:
+    print("Il est quand même un peu plus con que ça")
+else:
+    print("Je suis d'accord avec toi il est bien con")
+
+
